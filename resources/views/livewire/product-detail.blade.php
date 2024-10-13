650fcb8da749b4818944fc2c5b88499b3623f9ad
Nico <nicopolidis_12@hotmail.com>
2024-10-13 10:01:49 +0200

    mobile improved

--- resources/views/livewire/product-detail.blade.php
+++ resources/views/livewire/product-detail.blade.php
@@ -29,7 +29,7 @@
         </div>
 
         <!-- Product Information -->
-        <div class="flex flex-col justify-between px-4 md:px-0 mt-2">
+        <div class="flex flex-col justify-between px-12 md:px-0 mt-2 md:w-full w-full md:mx-0 mx-auto">
             <div>
                 @if($product->out_of_stock == false)
                     <p class="text-l text-white my-6 bg-green-600 rounded px-2 inline-block">{{ trans('pages/home.label-product-stock-available') }}</p>
@@ -40,20 +40,20 @@
 
                 <p class="text-gray-600 mt-4 w-4/5">{{ $product->description_1 }}</p>
 
-                <div class="text-lg font-semibold text-gray-800 mt-6">
+                <div class="text-xl font-semibold text-gray-800 mt-6">
                 {{ trans('pages/product-detail.label-price') }}: {{ number_format($specificPrice, 2) }}€
                 </div>
             </div>
 
             <!-- Quantity Selector and Add to Cart Button on the Same Row -->
-            <div class="flex items-center justify-between mt-8 space-x-4">
-                <div class="flex flex-col space-y-4"> <!-- Stacking the label, input, and button vertically -->
+            <div class="flex items-center justify-between mt-8 space-x-4 w-full">
+                <div class="flex flex-col space-y-4 w-full md:w-4/5"> <!-- Stacking the label, input, and button vertically -->
 
                     <!-- Size Variations Select Field -->
                     @if(!empty($productSizeVariations) && count($productSizeVariations) > 0)
                         <div class="flex flex-col">
-                            <label for="size" class="text-gray-700">{{ trans('pages/product-detail.label-select-product-size') }}:</label>
-                            <select id="size"  wire:change="updateSelectedSize($event.target.value)" wire:model="selectedSize" class="border border-gray-300 rounded-lg px-4 py-2 w-48">
+                            <label for="size" class="text-gray-700 mb-4">{{ trans('pages/product-detail.label-select-product-size') }}:</label>
+                            <select id="size"  wire:change="updateSelectedSize($event.target.value)" wire:model="selectedSize" class="border border-gray-300 rounded-lg px-4 py-2 w-full">
                                 @foreach($productSizeVariations->sortBy('order') as $Sizevariation)
                                     <option value="{{ $Sizevariation->id }}">{{ $Sizevariation->size_name }}</option>
                                 @endforeach
@@ -64,8 +64,8 @@
                         <!-- Size Variations Select Field -->
                         @if(!empty($productSizeVariations) && count($productSizeVariations) > 0)
                         <div class="flex flex-col mt-4">
-                            <label for="quantity" class="text-gray-700">{{ trans('pages/product-detail.label-select-product-quantity') }}:</label>
-                            <select id="quantity" wire:model="selectedQuantity" wire:change="updateSelectedQuanitity($event.target.value)" class="border border-gray-300 rounded-lg px-4 py-2 w-48">
+                            <label for="quantity" class="text-gray-700 mb-4">{{ trans('pages/product-detail.label-select-product-quantity') }}:</label>
+                            <select id="quantity" wire:model="selectedQuantity" wire:change="updateSelectedQuanitity($event.target.value)" class="border border-gray-300 rounded-lg px-4 py-2 w-full">
                                 @foreach($test as $testIndividual)
                                     <option value="{{ $testIndividual["id"] }}" >{{$testIndividual["quantity_name"]}}</option>
                                 @endforeach
@@ -79,15 +79,15 @@
                         @endif
                     @endif
                     <!-- Add to Cart Button -->
-                    <div class="flex items-center space-x-4 mt-8">
+                    <div class="flex items-center space-x-6 mt-8">
                         <!-- Quantity Input Field -->
-                        <div class="flex items-center space-x-2">
-                            <input type="number" id="quantity" wire:model="specificQuantity" class="border border-gray-300 rounded-lg px-4 py-2 w-20 text-center" min="1" value="1">
+                        <div class="flex items-center space-x-2 mt-4">
+                            <input type="number" id="quantity" wire:model="specificQuantity" class="border border-gray-300 rounded-lg px-4 py-2 w-20 text-center h-12" min="1" value="1">
                         </div>
 
                         <!-- Add to Cart Button -->
                         <button wire:click="addToCart" 
-                                class="bg-button-color-1 text-white px-6 py-3 rounded-lg hover:bg-sky-800 hover:text-button-color-1-hover 
+                                class="mt-4 w-full bg-button-color-1 text-white px-6 py-3 rounded-lg hover:bg-sky-800 hover:text-button-color-1-hover 
                                 {{ $product->out_of_stock ? 'opacity-50 cursor-not-allowed' : '' }}" 
                                 {{ $product->out_of_stock ? 'disabled' : '' }}>
                             {{ trans('pages/product-detail.button-add-to-cart') }}
